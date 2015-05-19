14b174a9dafe739efd27c6e6d83807396fdfe502
Logan Engstrom <meadowstream@users.noreply.github.com>
2015-05-18 23:34:12 -0400

    addedg

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,44 @@
+// struct Printer {
+// 	indent:1, // # of tabs added for each additional recursive level
+// 	width:80,
+// 	depth:Option::None
+// 	// stream:Option::None // Object is a stream
+// }
+
+struct PPrinter {
+	indent:u16,
+	width:u16,
+	depth:Option
+	stream:Option
+}
+
+impl PPrinter {
+	fn width(&mut self, width : u16) -> &mut PPrinter {
+		self.width = width
+		self
+	}
+
+	fn indent(&mut self, indent : u16) -> &mut PPrinter {
+		self.indent = indent
+		self
+	}
+
+	fn depth(&mut self, depth : Option) -> &mut PPrinter {
+		self.depth = depth
+		self
+	}
+
+	fn stream(&mut self, stream : Option) -> &mut PPrinter {
+		self.stream = stream
+		self
+	}
+}
+
+macro_rules! PPrint {
+	() => ()
+}
+
+
 #[test]
 fn it_works() {
 }
